3f2000b064edde01e9c19a124e5eb72aabe5f9be
i12arrae <i12arrae@uco.es>
2020-04-24 12:09:47 +0200

    Contexto de la base de datos y modelo de la tabla Mensajes.

---
+++ taskslist/Context/TestContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using taskslist.Models;
+
+namespace taskslist.Context
+{
+    public partial class TestContext : DbContext
+    {
+        public TestContext()
+        {
+        }
+
+        public TestContext(DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Mensajes> Mensajes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost;Database=TestDB;User Id=TestDbUser;Password=S4lm0r3j0.C0rd0b35");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Mensajes>(entity =>
+            {
+                entity.HasKey(e => e.MensajeId)
+                    .HasName("PK__Mensajes__6B304DCDBE52DD52");
+
+                entity.Property(e => e.MensajeId).HasColumnName("mensaje_id");
+
+                entity.Property(e => e.FechaEnvio)
+                    .HasColumnName("fecha_envio")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Mensaje).HasColumnName("mensaje");
+
+                entity.Property(e => e.Remitente)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ taskslist/Models/Mensajes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace taskslist.Models
+{
+    public partial class Mensajes
+    {
+        public long MensajeId { get; set; }
+        public string Mensaje { get; set; }
+        public DateTime FechaEnvio { get; set; }
+        public string Remitente { get; set; }
+    }
+}
