787bbc6aaf9dd509fc73af054b5ee399acabf38f
i12arrae <i12arrae@uco.es>
2020-04-25 11:03:30 +0200

    repaso del código

--- taskslist/wwwroot/js/chat.js
+++ taskslist/wwwroot/js/chat.js
@@ -1,29 +1,35 @@
 "use strict";
+$(function() {
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+    var $sendButton = $("#sendButton");
+    $sendButton.prop("disabled", true);
 
-//Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
+    $sendButton.on("click", function(event) {
+        var $userInput = $("#userInput");
+        var user = $userInput.val();
+        var $messageInput = $("#messageInput");
+        var message = $messageInput.val();
+        connection.invoke("SendMessage", user, message)
+            .then(function() {
+                $messageInput.val("");
+            })
+            .catch(function(err) {
+                console.error(err);
+            });
 
-connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + ": " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
-});
-
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
+        event.preventDefault();
     });
-    event.preventDefault();
+
+    var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+    connection.on("ReceiveMessage", function (user, message) {
+        $("#messagesList").append("<li>" + user + ": " + message + "</li>")
+    });
+
+    connection.start()
+        .then(function () {
+            $("#sendButton").prop("disabled", false);
+        }).catch(function (err) {
+            console.error(err);
+        });
 });
